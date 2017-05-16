e71d81fa1e8024b1ff0c4b94e833b3bff14a0b8c
Axel Bock <mr.axel.bock@gmail.com>
2017-05-16 16:56:52 +0200

    add dns entry template for app elb

--- variables.tf
+++ variables.tf
@@ -3,6 +3,14 @@
 
 variable "cname_prefix" {}
 
+variable "app_dns_name" {
+  default = "elbapp"
+}
+
+variable "app_subdomain_name" {
+  default = "elbtest"
+}
+
 # ============================================================================
 # required settings
 
@@ -163,10 +171,32 @@ variable "rupd_type" {
 # ============================================================================
 # docker settings
 
+# N/A
 
 # ============================================================================
 # other stuff
 
+# see here:
+# http://docs.aws.amazon.com/general/latest/gr/rande.html#elasticbeanstalk_region
+# don't change. it's static.
+variable "elb_hosted_zones" {
+  type    = "map"
+  default = {
+    "us-east-2"      = "Z14LCN19Q5QHIC"
+    "us-east-1"      = "Z117KPS5GTRQ2G"
+    "us-west-1"      = "Z1LQECGX5PH1X"
+    "us-west-2"      = "Z38NKT9BP95V3O"
+    "ca-central-1"   = "ZJFCZL7SSZB5I"
+    "ap-south-1"     = "Z18NTBI3Y7N9TZ"
+    "ap-northeast-2" = "Z3JE5OI70TWKCP"
+    "ap-southeast-1" = "Z16FZ9L249IFLT"
+    "ap-southeast-2" = "Z2PCDNR3VC2G1N"
+    "ap-northeast-1" = "Z1R25G3KIG2GBW"
+    "eu-central-1"   = "Z1FRNW7UH4DEZJ"
+    "eu-west-1"      = "Z2NYPWQ7DFZAZH"
+    "eu-west-2"      = "Z1GKAAAUGATPF1"
+    "sa-east-1"      = "Z10X7K2B4QSOFV"
+  }
+}
 
-# tbd
 
